9598e555a057315babe38436998df01a79eef572
ChaturvediSulabh <sulabhchaturvedi08@gmail.com>
2019-09-29 21:32:43 +0100

    intialize class with client and update method with args[refractor]

--- twetcher.rb
+++ twetcher.rb
@@ -1,32 +1,26 @@
 require 'twitter'
-require 'pry'
 require 'json'
 require 'yaml'
 
 class Twetcher
   
-  attr_accessor(:client, :tags, :count)
+  attr_accessor(:client)
  
-  def tw_search
-    client = set_twitter_client
-    @tags.each do |tag|
+  def tw_search(tags, count)
+    tags.each do |tag|
       puts "[#{tag}]"
       tag = "#"+tag+" -rt"
-      tweets = client.search(tag, result_type: "recent").take(@count)
+      tweets = @client.search(tag, result_type: "recent").take(count)
       tweets.each do |tweet|
        puts "\t#{tweet.full_text}" 
       end
        File.write('tweets.yml', YAML.dump(tweets))
     end
   end
-
-  def initialize(tags, count)
-    @tags, @count = tags, count
-  end
-
+  
   private
-  def set_twitter_client
-    client = Twitter::REST::Client.new do |config|
+  def initialize
+    @client = Twitter::REST::Client.new do |config|
       if File.file?("./private/secrets.json")
         file = File.read("./private/secrets.json")
         auth_hash = JSON.parse(file)
@@ -36,9 +30,9 @@ class Twetcher
         config.access_token_secret = auth_hash["config.access_token_secret"]
       end
     end
-    return client
+    return @client
   end
 end
 
-my_search = Twetcher.new(["kubernetes", "AWS", "DevOps", "cicd"], 3)
-my_search.tw_search
+my_search = Twetcher.new
+my_search.tw_search(["kubernetes", "AWS", "DevOps", "cicd"], 3)
